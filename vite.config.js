f4c08f08435e6a76a105d02cb619445d7272ca00
Travis <travisorams@gmail.com>
2025-10-17 20:15:41 +1300

    Vite bugfix3

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // IMPORTANT: replace with your actual repo name
-  base: process.env.GITHUB_ACTIONS ? '/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/-pokemon-research/' : '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
