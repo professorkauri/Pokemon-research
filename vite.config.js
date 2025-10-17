83380c3ad9d09e5e50c73b5110a176400684686f
Travis <travisorams@gmail.com>
2025-10-17 20:41:59 +1300

    Vita bugfix attempt 5

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-
 export default defineConfig({
-  // IMPORTANT: replace with your actual repo name
   base: process.env.GITHUB_ACTIONS ? '/Pokemon-research/' : '/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 });
