b081f5d0b786326369d0fb789d28243f1a2b67bb
Travis <travisorams@gmail.com>
2025-10-17 20:49:02 +1300

    Vite bugfix 6

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/Pokemon-research/' : '/',
+  base: '/Pokemon-research/',   // set this explicitly while testing
 });
