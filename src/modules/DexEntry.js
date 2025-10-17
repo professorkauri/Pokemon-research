c62143e5286a80e2d268c2251a17a76b60e98e7e
Travis <travisorams@gmail.com>
2025-10-18 09:02:12 +1300

    Miissed files when saving

--- src/modules/DexEntry.js
+++ src/modules/DexEntry.js
@@ -1,8 +1,6 @@
-import { useSidecarCSS } from '../utils/css.js';
 import { highlightHTMLMulti } from '../utils/search.js';
 import { addDexToNotepad } from '../overlays/NotepadOverlay.js';
 
-useSidecarCSS(import.meta.url);
 
 /** Single source of truth for Dex entry cards */
 export function DexEntry({ gameId, gameTitle, entryText, regionalDexNumber, colorHex = '#888888', terms = [], pokemon = null, game = null }) {
